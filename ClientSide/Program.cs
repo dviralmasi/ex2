1a97fa88feb9784e77493b6403253012fdc245b6
דביר אלמסי <dviralmasi@gmail.com>
2017-05-04 11:12:41 +0300

    first commit

---
+++ ClientSide/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net;
+
+namespace View
+{
+    class Program
+    {
+        /// <summary>
+        /// the main of the client
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //int port = int.Parse(ConfigurationManager.AppSettings["Port"]);
+            //IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            Client client = new Client();
+            client.startGame();
+            //keep the console open
+            Console.ReadKey();
+        }
+    }
+}
